08f372b95a665f8ef1390ce4ff13f99424a03e29
Zhenghci <344304180@qq.com>
2021-05-17 13:02:58 +0800

    1

---
+++ provision.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+echo 'Start!'
+
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+
+cd /vagrant
+
+sudo apt-get update
+sudo apt-get install tree
+
+if ! [ -e /vagrant/mysql-apt-config_0.8.15-1_all.deb ]; then
+	wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
+fi
+
+sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb
+sudo DEBIAN_FRONTEND=noninteractivate apt-get install -y mysql-server
+sudo apt-get install -y libmysqlclient-dev
+
+if [ ! -f "/usr/bin/pip" ]; then
+  sudo apt-get install -y python3-pip
+  sudo apt-get install -y python-setuptools
+  sudo ln -s /usr/bin/pip3 /usr/bin/pip
+else
+  echo "pip3"
+fi
+
+pip install --upgrade setuptools -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install --ignore-installed wrapt -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -U pip -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+
+# 设置mysql的root账户的密码为yourpassword
+sudo mysql -u root << EOF
+	ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '111111';
+	flush privileges;
+	show databases;
+	CREATE DATABASE IF NOT EXISTS twitter;
+EOF
+
+echo 'All Done!'
